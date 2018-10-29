4dfa5bc32f4e892a269ba8b724903c818d2d6537
Alex Hill <alex.hill@gmail.com>
2018-10-29 17:09:02 +0000

    add minified files

--- README.md
+++ README.md
@@ -4,9 +4,13 @@ collapse functionality.
 See documentation here: https://getbootstrap.com/docs/4.0/components/collapse/s
 
 To re-create from scratch, run `npm install`
-followed by `gulp build`.
+followed by `gulp build`. This creates 4 files:
+* `dist/css/collapse.css`
+* `dist/css/collapse.min.css`
+* `dist/css/collapse.js`
+* `dist/css/collapse.min.js`
 
 To use:
-1. include `dist/css/collapse.css` as a stylesheet
-2. include `jquery` in your scripts
-2. include `dist/js/collapse.js` in your scripts
+1. include `dist/css/collapse.css` or `dist/css/collapse.min.css` as a stylesheet
+2. include `jquery` in your page scripts
+2. include `dist/js/collapse.js` or `dist/js/collapse.min.js` in your page scripts
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,33 @@
 'use strict';
 
 const gulp = require('gulp'),
-      sass = require('gulp-sass'),
-      concat = require('gulp-concat');
+    sass = require('gulp-sass'),
+    minify = require('gulp-clean-css'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat');
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
     return gulp.src('./collapse.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(minify({compatibility: 'ie8'}))
+        .pipe(rename({
+            extname: ".min.css"
+        }))
         .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('js', function () {
-    return gulp.src(['./node_modules/bootstrap/js/dist/util.js','./node_modules/bootstrap/js/dist/collapse.js'])
+    return gulp.src(['./node_modules/bootstrap/js/dist/util.js', './node_modules/bootstrap/js/dist/collapse.js'])
         .pipe(concat('collapse.js'))
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(uglify())
+        .pipe(rename({
+            extname: ".min.js"
+        }))
         .pipe(gulp.dest('./dist/js'));
 });
 
